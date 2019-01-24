fc3428e20d5a9954420f4a99bfb8596b8c507278
Octopussyzz <spudd.otw@gmail.com>
2019-01-24 21:41:15 +0100

    Surprise

--- lib/game_router.rb
+++ lib/game_router.rb
@@ -1,25 +1,29 @@
 require 'game_controller'
-
+require 'colorize'
+require 'colorized_string'
+require 'rick'
 class GameRouter
 
   def perform
+    puts `clear`
     puts "BIENVENUE DANS LE TIC-TAC-TOE DE LA MUERTE (la mort en espagnol)"
     while true
-
       #on affiche le menu
-      puts "Tu veux te faire une partie ?"
-      puts "1. Oui"
+      puts "\nTu veux te faire une partie ?"
+      puts "\n1. Oui \u{1f3b0}"
       #rejouer contre le même joueur
-      #affichage d'une page bonus
-      puts "4. Je vous emmerde et je rentre à ma maison"
+      puts "\n2. Tu aimes les surprises ? \u{1f381}".blink #affichage d'une page bonus
+      puts "\n3. Je vous emmerde et je rentre à ma maison \u{1f3d8}"
       params = gets.chomp.to_i #on attend le choix de l'utilisateur
 
       case params #en fonction du choix
       when 1
         puts "Ok Séraphin, tu es très très sympa alors va pour une partie.\n\n"
         @game = Game.new.play
-      when 4
-        puts "À bientôt !"
+      when 2
+        Rick.new.blurp
+      when 3
+        Cartman.new.vous_emmerde
         break #ce "break" permet de sortir de la boucle while.
       else
         puts "Ce choix n'existe pas, merci de ressayer" #si l'utilisateur saisit une entrée non prévue.
---
+++ lib/rick.rb
@@ -0,0 +1,125 @@
+class Rick
+	def blurp
+		puts `clear`
+
+		puts "\n\n\n\n                 BLUUUUUUUUURP WUBA BLURP LUBBA DUB BLURRRP DUB !"
+
+puts "                                                                                                                                                      "
+sleep(0.05)
+puts "                                                                                                                                                      "
+sleep(0.05)
+puts "                                                                                                                                                      "
+sleep(0.05)
+puts "                                                                                                                                                      "
+sleep(0.05)
+puts "                                                                                                                                                      "
+sleep(0.05)
+puts "                                                                                                                                                      "
+sleep(0.05)
+puts "                                                                                                                                                      "
+sleep(0.05)
+puts "                                                                                                                                                      "
+sleep(0.05)
+puts "                                                                                                                                                      "
+sleep(0.05)
+puts "                                                                                `                                                                     "
+sleep(0.05)
+puts "                                                                                ho.       `s`                                                         "
+sleep(0.05)
+puts "                                                                                dmms.    .hm:                                                         "
+sleep(0.05)
+puts "                                                                               `mmmmms. -dmmo                                                         "
+sleep(0.05)
+puts "                                                                        :+/:-.`+mmmmmmmymmmmy                                                         "
+sleep(0.05)
+puts "                                                                        `ymmmmdmmmmdmmmmmmdmy/+ss`                                                    "
+sleep(0.05)
+puts "                                                                         `ymmmmmmddNNNNmmNNddmmd.                                                     "
+sleep(0.05)
+puts "                                                                          `hmmmmhNmddhddddhddyh.                                                      "
+sleep(0.05)
+puts "                                                                     .-/+shmmmmhNNdmdmmNNNNmdh+:.                                                     "
+sleep(0.05)
+puts "                                                                    -sdmmmmmmmmdNNmmNMNmmNmmmddds-                                                    "
+sleep(0.05)
+puts "                                                                      `:sdmmmmhNNNdMMMMMNyNMMMMs`                            -`                       "
+sleep(0.05)
+puts "                                                                         -ommmhNNNdMMNyMmhNMMhNo                             d-                       "
+sleep(0.05)
+puts "                                                                      .ohdmmmmdNNNmmmmdmNNhmNhs`                             N:                       "
+sleep(0.05)
+puts "                                                                      `-ohmmdNNNNNmmmmNNdNdmmso`      `                   `.`N+`                      "
+sleep(0.05)
+puts "                                                                         `odmNNNNdddmNNNNddNm:       -h                 -sydhmmd.                     "
+sleep(0.05)
+puts "                                                                         :yhhhNNdN:-:+ydddhs/:y      od                 ohmNNNNy`                     "
+sleep(0.05)
+puts "                                                                           `oddNdm+....---...:y      hh                 `+NNNNo`                      "
+sleep(0.05)
+puts "                                                                           `...hNdmds+/:::/+o/`   +s/dh-.                dNNm/                        "
+sleep(0.05)
+puts "                                                                               `+mmNNNNmmh:.`   `/hdNNmmh.             .odmd/                         "
+sleep(0.05)
+puts "                               ``.---...``                               :+o+++/smNmms++:`      :ddmNNNNN-            /mNdhd+                         "
+sleep(0.05)
+puts "                           `.---:::::::::--`                            yMMMmmNdddddhdmmmh/.     smNNNNh-           `sMMMMm/                          "
+sleep(0.05)
+puts "                          .-:::::::::///:::-.                          +MMMMMmNdmmmmhNmMMMMNh/.   +dmm+`           -dMMMMy.                           "
+sleep(0.05)
+puts "                        `-::::::/+yhdddddhs+:-                        `NMmMMMMmhmmmmdMdMMmNMMMNh/ohmho           `+NMMMN/                             "
+sleep(0.05)
+puts "                       .-:::::+ydmmmmmmmmmmmhs-.                      oMMNdNMMMNdmmdmMmNM:.+hNMmNMmddo          .hMMMMh.                              "
+sleep(0.05)
+puts "                      `::::::ymmmmdddddhdmmmmmmdh/                   .NMMmNmNMMMNdmhMMMds    -dNMMMNmy:`       /mMMMN+`                               "
+sleep(0.05)
+puts "                      .::::/dmmmmmmmmmmmhmmdmmmdmm.                  sMMMNmMNmMMMMdhMMMd`   -hMMMMdmMMMdo-`  `sMMMMh-                                 "
+sleep(0.05)
+puts "                      .::::dmmmmhmmmmyymhdmhNNNmNd`                 .NMMMMdMMhdMMMMmNmh+  `+NMMMNo`-smMMMNd+:dMMMNo`                                  "
+sleep(0.05)
+puts "                      .:::+mmmmmmmNNNNNmdmmddmNmm/                  yMMNmNNMmdmdNMMMN/   .hMMMMd:    `/hNMMMNMMMd-                                    "
+sleep(0.05)
+puts "                       -::+mmmmmmmmmmmmmmmddhmmmm/                 -MMMdMMMMhmmmdNMMMN/ /mMMMMs`        .odMMMNo`                                     "
+sleep(0.05)
+puts "                        -:hmmmmmmmmmmmmmmmmmmmmmm.                 hMMMdMMMdmmmmmdmMMMNhMMMMm/            `:os-                                       "
+sleep(0.05)
+puts "                         `sddmmmmmmmmmmmddddddmms                 :MMMMmNMNhmmmmmmdmMMMMMMMy.                                                         "
+sleep(0.05)
+puts "                           `-smmmmmmmmmmmmmmmmd+`        `:       dMMMMMmMdmmmmmmhNMdNMMMN+                                                           "
+sleep(0.05)
+puts "                              -odmmmmmmmmmmmd/.`.-:/++//:/o`...../MMMMMMdNdmmmmmmhMNm/hmh-                                                            "
+sleep(0.05)
+puts "                               +dmdddddddmmmmh+:odNmdosyyys/yddysdMMMMMMdhmmmmmmmdNmm``.```                                                           "
+sleep(0.05)
+puts "                             `:mNNNNNNNNNNNmhhmh+dho+/+hmmmooo+++MMMMMMMdmmmmmmmmdmMyohdo+s://:-.-.`                                                  "
+sleep(0.05)
+puts "                         .-:::sNNNNNNNNNNmdhdymNmsssssodmmy+/+//sNNMMMMMhmmmmmmmdmMM+:+yhs/smmy//dho/-                                                "
+sleep(0.05)
+puts "                       `:/smsosmNNNmmNNNNyydmdhNNmyssoodmys/+o+//++ydddhddhyyhddshdm::/oso//++/+oMMyos/`                                              "
+sleep(0.05)
+puts "                       ::yys++oymNNNddNNNhhmmhhdmNmo/+hmm+o+///+oo+/:::///:::://:::/+o+//:::::::+yyyyy++                                              "
+sleep(0.05)
+puts "                      -:/MNs+:+symNNNddyyosmh++/ohdy+smmd//osssso+/::::::::::///++/+so+/::::///+oyhys/:o-                                             "
+sleep(0.05)
+puts "                     `:/dMhs+/+oo+hmNmd/:/hmd+///++//syyysssso//::/++///:::::+osoossss+::::/dmo/+sso/:/s:                                             "
+sleep(0.05)
+puts "                     .:yMN/ohdy:::/yho+/:+ooo/:+sssssssssssss/:::/o+//+++++/////::/++o++oo++sysooo+++oo+`                                             "
+sleep(0.05)
+puts "                     -:omNhddy+/:///++/++o+++++osssssso++oso+////o++ossssssssoo++///dNMMNmmds+//ssoo+:.                                               "
+sleep(0.05)
+puts "                     .::/+oyy+oooymhso+ssssso+/////sso/:+NMNh/oo++++/++syhssoymyoosdMMdysooo++::-.`                                                   "
+sleep(0.05)
+puts "                      .::::+o/:/++oddo/dd+oydyy//+ymNyo:/ohdy//+o+osydyyhdo/:mMMmddMMysss+.`                                                          "
+sleep(0.05)
+puts "                        ````````.::/os+////smNMNNNNNNNy::/++//mMMMNNMMNysss//mMMMMMNs/so-                                                             "
+sleep(0.05)
+puts "                                   `-+osssssssyhyysssso+//////sNMMMMMmyo++/:+mmhhyyso+:`                                                              "
+sleep(0.05)
+puts "                                       .-:/+oooooooooosssooosssoshhhso////+oso/-....`                                                                 "
+sleep(0.05)
+puts "                                              `````````.-/+osssso////+oossso:`                                                                        "
+sleep(0.05)
+puts "                                                            `--/++ooooo+/:.                                                                           "
+sleep(0.05)
+
+end
+end
